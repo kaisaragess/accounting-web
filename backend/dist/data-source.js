76c3e38b8aa2eff585097445c22c245d6097117c
faroh <farohafalasi12@gmail.com>
2025-10-31 17:11:22 +0700

    buku besar, journals, lporn laba rugi pdf, trial, transaction

--- backend/dist/data-source.js
+++ backend/dist/data-source.js
@@ -4,11 +4,12 @@ exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 const config = {
     type: "postgres",
-    host: 'db.pagonila.id', // <-- host database
+    host: '103.217.146.217', // <-- host database
     port: 47001,
     username: 'lrntz_gambitsch',
     password: '6a2bfa7b8a',
-    database: 'kasir_bakso_tiga_f984eea72f4649cb9b5072748efd34ca', // <-- nama database
+    database: 'accounting_web', // <-- nama database
+    // database: 'kasir_bakso_tiga_f984eea72f4649cb9b5072748efd34ca', // <-- nama database
     synchronize: false,
     logging: false,
     migrations: [
